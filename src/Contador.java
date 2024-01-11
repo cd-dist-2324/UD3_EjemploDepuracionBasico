618b19113844fdeebc6d4a66f2fa0a9d434472da
cd-dist-2324 <cd.dist.2324@gmail.com>
2024-01-11 20:00:39 +0100

    ejemplo debug

---
+++ src/Contador.java
@@ -0,0 +1,18 @@
+
+public class Contador {
+    private int resultado = 0;
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void contar() {
+        for (int i = 0; i < 100; i++) {
+            i=99;
+            int resultado_antes_modificacion = resultado;
+            resultado = resultado + i + 1;
+            // ++
+            System.out.printf("resultado + i + 1 = %d + %d + 1 = %d \n", resultado_antes_modificacion, i, resultado);
+        }
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) throws Exception {
+        Contador contador = new Contador();
+		contador.contar();
+		System.out.println("Cuenta: " + contador.getResultado());
+	
+    }
+}
